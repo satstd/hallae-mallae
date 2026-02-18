777c6970eea53087cbb535c11f4075da4e8ae34e
K-KY <kyuyoungk@naver.com>
2026-02-18 23:10:46 +0900

    Initial Commit

---
+++ src/main/java/com/java/hallaemallae/HallaeMallaeApplication.java
@@ -0,0 +1,13 @@
+package com.java.hallaemallae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HallaeMallaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HallaeMallaeApplication.class, args);
+    }
+
+}
