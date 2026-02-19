2af0d24d89d0a70eade8562145f1db1a178511cc
K-KY <kyuyoungk@naver.com>
2026-02-19 16:12:14 +0900

    test: 사용자 서비스 테스트

---
+++ src/test/java/com/java/hallaemallae/member/service/MemberServiceImplTest.java
@@ -0,0 +1,82 @@
+package com.java.hallaemallae.member.service;
+
+import com.java.hallaemallae.member.dto.MemberDto;
+import com.java.hallaemallae.member.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+class MemberServiceImplTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Test
+    @Transactional
+    void saveMember() {
+
+        MemberDto.Request dto = MemberDto.Request.builder()
+                .loginId("loginId")
+                .password("password")
+                .nickname("nickname")
+                .build();
+
+        Member member = memberService.saveMember(dto);
+        assertThat(member.getNickname()).isEqualTo(dto.getNickname());
+    }
+
+    @Test
+    @Transactional
+    void updateMember() {
+        MemberDto.Request dto = MemberDto.Request.builder()
+                .loginId("loginId")
+                .password("password")
+                .nickname("nickname")
+                .build();
+
+        Member member = memberService.saveMember(dto);
+
+
+        member.changeNickname("newNickname");
+        assertThat(member.getNickname()).isEqualTo("newNickname");
+
+        String password = member.getPassword();
+        member.changePassword("newPassword");
+        assertThat(member.getPassword()).isNotEqualTo(password);
+
+    }
+
+    @Test
+    @Transactional
+    void existMemberTest() {
+        MemberDto.Request dto = MemberDto.Request.builder()
+                .loginId("loginId")
+                .password("password")
+                .nickname("nickname")
+                .build();
+
+        memberService.saveMember(dto);
+
+        assertThat(memberService.existByLoginId(dto.getLoginId())).isTrue();
+    }
+
+    @Test
+    @Transactional
+    void deleteMemberTest() {
+        MemberDto.Request dto = MemberDto.Request.builder()
+                .loginId("loginId")
+                .password("password")
+                .nickname("nickname")
+                .build();
+        memberService.saveMember(dto);
+
+        memberService.deleteMember(dto);
+
+        assertThat(memberService.existByLoginId(dto.getLoginId())).isFalse();
+    }
+}
