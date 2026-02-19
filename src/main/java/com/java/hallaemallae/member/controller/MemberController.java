f575f0ea447bad85cad94d8896875e0bffd50a3c
K-KY <kyuyoungk@naver.com>
2026-02-19 16:12:28 +0900

    test: 사용자 컨트롤러

---
+++ src/main/java/com/java/hallaemallae/member/controller/MemberController.java
@@ -0,0 +1,24 @@
+package com.java.hallaemallae.member.controller;
+
+import com.java.hallaemallae.member.dto.MemberDto;
+import com.java.hallaemallae.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity<MemberDto.Response> saveMember(@RequestBody MemberDto.Request dto) {
+        return ResponseEntity.ok().body(MemberDto.of(memberService.saveMember(dto)));
+    }
+
+    @PatchMapping
+    public ResponseEntity<MemberDto.Response> updateMember(MemberDto.Update dto) {
+        return ResponseEntity.ok().body(MemberDto.of(memberService.updateMember(dto)));
+    }
+}
