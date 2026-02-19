bba9d03a66e9f64934124dc404539f1d0afccc99
K-KY <kyuyoungk@naver.com>
2026-02-19 17:18:51 +0900

    feat: 사용자 상태 컨트롤러

---
+++ src/main/java/com/java/hallaemallae/member/controller/MemberStatusController.java
@@ -0,0 +1,30 @@
+package com.java.hallaemallae.member.controller;
+
+import com.java.hallaemallae.member.dto.MemberDto;
+import com.java.hallaemallae.member.dto.MemberStatusDto;
+import com.java.hallaemallae.member.service.MemberStatusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberStatusController {
+    private final MemberStatusService memberStatusService;
+
+    @PostMapping
+    public ResponseEntity<MemberStatusDto.Response> saveMemberStatus(MemberDto.Request memberDto) {
+        return ResponseEntity.ok().body(
+                MemberStatusDto.of(memberStatusService.saveMemberStatus(memberDto))
+        );
+    }
+
+    @PatchMapping
+    public ResponseEntity<MemberStatusDto.Response> updateMemberStatus(MemberStatusDto.Update statusDto) {
+        return ResponseEntity.ok().body(
+                MemberStatusDto.of(memberStatusService.updateMemberStatus(statusDto))
+        );
+    }
+}
