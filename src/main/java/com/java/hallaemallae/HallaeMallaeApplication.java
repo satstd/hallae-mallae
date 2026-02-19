ece1c75d56e385db7795c9b3fce4e2b807e461f9
K-KY <kyuyoungk@naver.com>
2026-02-19 15:59:17 +0900

    build: 날짜 넣기위한 어노테이션 추가

--- src/main/java/com/java/hallaemallae/HallaeMallaeApplication.java
+++ src/main/java/com/java/hallaemallae/HallaeMallaeApplication.java
@@ -2,8 +2,10 @@ package com.java.hallaemallae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HallaeMallaeApplication {
 
     public static void main(String[] args) {
