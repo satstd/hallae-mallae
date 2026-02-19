7cc983cee59935cd77090b9eb5375b1756c03cea
K-KY <kyuyoungk@naver.com>
2026-02-19 17:11:47 +0900

    feat: 사용자 상태 엔티티

---
+++ src/main/java/com/java/hallaemallae/member/entity/MemberStatus.java
@@ -0,0 +1,37 @@
+package com.java.hallaemallae.member.entity;
+
+import com.java.hallaemallae.util.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access=AccessLevel.PROTECTED)
+@AllArgsConstructor(access=AccessLevel.PROTECTED)
+public class MemberStatus extends BaseTimeEntity {
+
+    @Id
+    private Long memberSeq;
+
+    @MapsId
+    @OneToOne
+    @JoinColumn(name = "memberSeq")
+    private Member member;
+
+    private Long totalGames;
+    private Long totalWins;
+    private Double reactionAverage;
+
+    public void changeTotalGames(Long totalGames) {
+        this.totalGames += totalGames;
+    }
+
+    public void changeTotalWins(Long totalWins) {
+        this.totalWins += totalWins;
+    }
+
+    public void changeReactionAverage(Double reactionAverage) {
+        this.reactionAverage = reactionAverage;
+    }
+}
