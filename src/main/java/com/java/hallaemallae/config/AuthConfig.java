97160bd508bacfa100a3cb0feedbb2a9f491235a
K-KY <kyuyoungk@naver.com>
2026-02-20 23:16:53 +0900

    feat: ObjectMapper 빈 등록

--- src/main/java/com/java/hallaemallae/config/AuthConfig.java
+++ src/main/java/com/java/hallaemallae/config/AuthConfig.java
@@ -1,5 +1,6 @@
 package com.java.hallaemallae.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,5 +12,10 @@ public class AuthConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 }
