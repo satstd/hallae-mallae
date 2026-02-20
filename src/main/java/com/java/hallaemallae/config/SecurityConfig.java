df99fa72787c863ac5d5e5a8a4ec38f0f506a1a5
K-KY <kyuyoungk@naver.com>
2026-02-20 23:19:17 +0900

    feat: 시큐리티 필터에 로그인 필터 등록

--- src/main/java/com/java/hallaemallae/config/SecurityConfig.java
+++ src/main/java/com/java/hallaemallae/config/SecurityConfig.java
@@ -1,21 +1,46 @@
 package com.java.hallaemallae.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.java.hallaemallae.domain.auth.service.JwtService;
+import com.java.hallaemallae.domain.auth.service.jwt.JwtAuthenticationFilter;
+import com.java.hallaemallae.domain.auth.service.jwt.JwtLoginFilter;
+import com.java.hallaemallae.domain.auth.service.jwt.JwtProvider;
+import com.java.hallaemallae.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final ObjectMapper objectMapper;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JwtService jwtService;
+    private final JwtProvider jwtProvider;
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        AuthenticationManager authenticationManager =
+                authenticationConfiguration.getAuthenticationManager();
+
+        JwtLoginFilter jwtLoginFilter =
+                new JwtLoginFilter(authenticationManager, jwtService, objectMapper);
+
         http
                 //CSRF 보호 비활성화
                 .csrf(AbstractHttpConfigurer::disable)
@@ -27,9 +52,16 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         //모든 경로 허용
-                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/login", "/auth")
+                        .permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterAt(jwtLoginFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(
+                        new JwtAuthenticationFilter(jwtProvider),
+                        UsernamePasswordAuthenticationFilter.class
+                )
+
 
                 //기본 로그인/인증 방식 비활성화
                 .formLogin(AbstractHttpConfigurer::disable)
@@ -37,4 +69,18 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            HttpSecurity http
+    ) throws Exception {
+        AuthenticationManagerBuilder builder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        builder
+                .userDetailsService(memberService)
+                .passwordEncoder(passwordEncoder);
+        return builder.build();
+    }
+
 }
