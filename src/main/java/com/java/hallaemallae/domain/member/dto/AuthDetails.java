2e6819b122dc300bcb2aa9e4e1e8633a4b03c335
K-KY <kyuyoungk@naver.com>
2026-02-20 23:15:48 +0900

    feat: 인증 성공시 생성할 사용자 인증 객체

---
+++ src/main/java/com/java/hallaemallae/domain/member/dto/AuthDetails.java
@@ -0,0 +1,48 @@
+package com.java.hallaemallae.domain.member.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class AuthDetails implements UserDetails {
+    private final Long memberSeq;
+    private final String loginId;
+    private final String password;
+    private final String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return loginId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
