8555a3a2fd3017640a96ddc493d6b2c875cc253e
K-KY <kyuyoungk@naver.com>
2026-03-09 15:47:46 +0900

    feat(user): userService와 UserDetailsService 서비스 구현체

---
+++ src/main/java/com/java/hallaemallae/domain/user/service/UserServiceImpl.java
@@ -0,0 +1,61 @@
+package com.java.hallaemallae.domain.user.service;
+
+import com.java.hallaemallae.domain.user.dto.UserDetail;
+import com.java.hallaemallae.domain.user.dto.UserRequestDto;
+import com.java.hallaemallae.domain.user.entity.User;
+import com.java.hallaemallae.domain.user.repository.UserRepository;
+import com.java.hallaemallae.domain.user.service.interfaces.UserService;
+import com.java.hallaemallae.global.exception.UserAlreadyExistException;
+import com.java.hallaemallae.global.exception.UserNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    @Override
+    public User signUp(UserRequestDto request) {
+        boolean exists = userRepository.existsUserByUsername(request.getUsername());
+        if (exists) {
+            throw new UserAlreadyExistException("이미 존재하는 사용자 아이디 입니다.");
+        }
+        User user = User.createUser(request, passwordEncoder);
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User findUserByUsername(String username) {
+        return userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException("일치하는 사용자를 찾을 수 없습니다. :" + username));
+    }
+
+    @Override
+    @Transactional
+    public User updateUser(UserDetail userDetail, UserRequestDto request) {
+        User user = userRepository.findById(userDetail.getUserId())
+                .orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없습니다."));
+        user.changePassword(request.getPassword());
+        user.changeNickname(request.getNickname());
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = findUserByUsername(username);
+        return new UserDetail(user.getUserId(), user.getUsername(),
+                user.getPassword(), user.getNickname(), user.getRole());
+    }
+}
