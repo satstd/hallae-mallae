44e8daac1d69368b79116d2329dd183e4773e76d
K-KY <kyuyoungk@naver.com>
2026-03-09 14:06:02 +0900

    feat(entity): 엔티티 공통 칼럼 BaseTimeEntity로 분리

---
+++ src/main/java/com/java/hallaemallae/global/entity/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package com.java.hallaemallae.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+
+@Setter
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+}
