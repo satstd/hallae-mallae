8754da948757b862cb7e3a81b3bc2cd9fb88891f
K-KY <kyuyoungk@naver.com>
2026-03-11 22:01:19 +0900

    feat(user): 로그인시 사용할 Dto

---
+++ src/main/java/com/java/hallaemallae/domain/auth/dto/LoginDto.java
@@ -0,0 +1,19 @@
+package com.java.hallaemallae.domain.auth.dto;
+
+public class LoginDto {
+    private String username;
+    private String password;
+
+    public LoginDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
