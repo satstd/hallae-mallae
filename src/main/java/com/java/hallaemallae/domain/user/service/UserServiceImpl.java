1b78f8b9635c5037097e73135a93b7630dd74741
K-KY <kyuyoungk@naver.com>
2026-03-11 22:04:51 +0900

    refactor(user): userDetailService를 구현체가 아니라 인터페이스가 상속하는 방식으로 변경

--- src/main/java/com/java/hallaemallae/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/java/hallaemallae/domain/user/service/UserServiceImpl.java
@@ -10,13 +10,12 @@ import com.java.hallaemallae.global.exception.UserAlreadyExistException;
 import com.java.hallaemallae.global.exception.UserNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl implements UserService, UserDetailsService {
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
--- src/main/java/com/java/hallaemallae/domain/user/service/interfaces/UserService.java
+++ src/main/java/com/java/hallaemallae/domain/user/service/interfaces/UserService.java
@@ -3,8 +3,9 @@ package com.java.hallaemallae.domain.user.service.interfaces;
 import com.java.hallaemallae.domain.user.dto.UserDetail;
 import com.java.hallaemallae.domain.user.dto.UserRequestDto;
 import com.java.hallaemallae.domain.user.entity.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     User signUp(UserRequestDto request);
     User findUserByUsername(String username);
     User updateUser(UserDetail userDetail, UserRequestDto request);
