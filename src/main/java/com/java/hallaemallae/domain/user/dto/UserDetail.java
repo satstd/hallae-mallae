03cdc0fd79fa93d5eb9bab3e6a6b47ec1d2e1961
K-KY <kyuyoungk@naver.com>
2026-03-11 21:41:52 +0900

    chore(user): 생성자 제거
    - 롬복 어노테이션으로 변경

--- src/main/java/com/java/hallaemallae/domain/user/dto/UserDetail.java
+++ src/main/java/com/java/hallaemallae/domain/user/dto/UserDetail.java
@@ -1,6 +1,7 @@
 package com.java.hallaemallae.domain.user.dto;
 
 import com.java.hallaemallae.domain.user.service.Role;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,20 +9,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
+@AllArgsConstructor
 public class UserDetail implements UserDetails {
     private final Long userId;
     private final String username;
     private final String password;
     private final String nickname;
     private final Role role;
-
-    public UserDetail(Long userId, String username, String password, String nickname, Role role) {
-        this.userId = userId;
-        this.username = username;
-        this.password = password;
-        this.nickname = nickname;
-        this.role = role;
-    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
